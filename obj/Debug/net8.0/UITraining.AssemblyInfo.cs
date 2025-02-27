b7e49abddeabd63293c5ad8dff989b54ae9445ac
Efengsi Rahmanto Zalukhu <fengsifeng123@gmail.com>
2025-02-27 12:24:35 +0700

    New Update

--- obj/Debug/net8.0/UITraining.AssemblyInfo.cs
+++ obj/Debug/net8.0/UITraining.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UITraining")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d62162573a2f6d135973a3e949134b3e3ef6fdf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0e011a9e145fb61a87467c8d4d4d51c7f38c1fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("UITraining")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UITraining")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
