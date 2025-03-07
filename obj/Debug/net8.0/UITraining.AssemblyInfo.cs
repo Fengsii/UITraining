2616cf761b041d507c2c5cad73c0eabbef56cccf
Efengsi Rahmanto Zalukhu <fengsifeng123@gmail.com>
2025-03-08 04:08:23 +0700

    Update Crud Supplier and Option

--- obj/Debug/net8.0/UITraining.AssemblyInfo.cs
+++ obj/Debug/net8.0/UITraining.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UITraining")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+655e1fbc8cca00cb1bf430d624fbad9732f9ce93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+566250473392b4ccf25701ef1bf9d57e2abbab79")]
 [assembly: System.Reflection.AssemblyProductAttribute("UITraining")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UITraining")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
