e293bd2849a4203768f9d719ed14272ba7973cea
Efengsi Rahmanto Zalukhu <fengsifeng123@gmail.com>
2025-03-12 10:21:18 +0700

    Update Login

--- obj/Debug/net8.0/UITraining.AssemblyInfo.cs
+++ obj/Debug/net8.0/UITraining.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UITraining")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4b7bebf6522b0ec8102635ffcca519544c449cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+193ae1f7b0230432137f367334833b8baea00bd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("UITraining")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UITraining")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
