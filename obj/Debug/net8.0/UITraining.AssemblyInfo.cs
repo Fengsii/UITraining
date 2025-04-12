958d765c7b4e049284cb2048324cd646f99201a2
Efengsi Rahmanto Zalukhu <fengsifeng123@gmail.com>
2025-04-12 14:10:54 +0700

    add

--- obj/Debug/net8.0/UITraining.AssemblyInfo.cs
+++ obj/Debug/net8.0/UITraining.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UITraining")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c45a89a0b119ee27458ff252fb39b91ff4ad8ddb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e656da2b39141509a1e89fc246ecdcc335ebfe83")]
 [assembly: System.Reflection.AssemblyProductAttribute("UITraining")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UITraining")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
